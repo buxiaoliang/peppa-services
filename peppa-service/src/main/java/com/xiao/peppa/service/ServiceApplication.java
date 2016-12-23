c34478f43ebdfe32a0971c909acdd3776e34dddb
xiaoliang.bu <xiaoliang.bu@hpe.com>
2016-12-23 17:14:26 +0800

    Separate two standalone services.

--- peppa-service/src/main/java/com/xiao/peppa/service/ServiceApplication.java
+++ peppa-service/src/main/java/com/xiao/peppa/service/ServiceApplication.java
@@ -1,7 +1,14 @@
 package com.xiao.peppa.service;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by bu on 12/22/2016.
  */
+@SpringBootApplication
 public class ServiceApplication {
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
 }
